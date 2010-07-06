d8a98ace45e646d8580f7890aef8aff7f8d667a4
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-07-06 07:24:06 +0000

    initial release of version 0.0.1 which does nothing
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mastertemplate/trunk@35403 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext "mastertemplate".
+#
+# Auto generated 09-05-2010 19:48
+#
+# Manual updates:
+# Only the data in the array - everything else is removed by next
+# writing. "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Static Template for Websites',
+	'description' => 'This extension has been created from the master template provided by the NRW user group.',
+	'category' => 'fe',
+	'author' => 'Franz Holzinger',
+	'author_email' => 'franz@ttproducts.de',
+	'shy' => '',
+	'dependencies' => 'statictemplates',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'alpha',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.0.1',
+	'constraints' => array(
+		'depends' => array(
+			'statictemplates' => '',
+			'typo3' => '4.4.0-4.4.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:4:{s:9:"ChangeLog";s:4:"6d46";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"doc/manual.sxw";s:4:"623f";}',
+	'suggests' => array(
+	),
+);
+
+?>
