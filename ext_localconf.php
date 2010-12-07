0c5a94d6e94048a4795bae2047e1c58421f8b791
franzholz <franzholz@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-12-07 12:28:00 +0000

    * Remove not needed master constants.txt, because this is part of the database record
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mastertemplate/trunk@40927 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,11 +2,4 @@
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
 
-if (TYPO3_MODE=='BE')	{
-
-	// allow the import of records into static_template
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['typo3/mod/tools/em/index.php']['checkDBinsert'][] = 'EXT:mastertemplate/hooks/class.tx_mastertemplate_hooks_em.php:&tx_mastertemplate_hooks_em';
-}
-
-
 ?>
