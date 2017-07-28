122683d5314d78d5c8ca198b492cd3a7a8ffea56
Franz Holzinger <holzinger@ttproducts.de>
2017-07-28 18:21:05 +0200

    * bug fix: make the images generated correctly by moving the Images folder into Documentation/ExtMasterTemplate/ * rename manual_html_2d0df56f.png into manual_install_extension.png * rename manual_html_m2986e6f2.png into manual_install_extension_2.png * rename manual_html_62e0db56.png into manual_css_styled_content.png

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,32 +1,35 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "mastertemplate".
-#
-#
-# Manual updates
-########################################################################
+/**
+ * Extension Manager/Repository config file for ext "mastertemplate".
+ *
+ * Auto generated 09-02-2016 11:40
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ */
 
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'Master Template',
 	'description' => 'This extension makes available the setup of the master template. Originally developed by the NRW user group.',
 	'category' => 'templates',
-	'author' => 'Franz Holzinger',
-	'author_email' => 'franz@ttproducts.de',
-	'shy' => '',
-	'dependencies' => '',
-	'conflicts' => '',
+	'shy' => 1,
+	'version' => '0.3.3',
 	'priority' => '',
+	'loadOrder' => '',
 	'module' => '',
 	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
+	'uploadfolder' => 1,
 	'createDirs' => '',
 	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
+	'clearcacheonload' => 1,
 	'lockType' => '',
+	'author' => 'Franz Holzinger',
+	'author_email' => 'franz@ttproducts.de',
 	'author_company' => 'jambage.com',
-	'version' => '0.3.2',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
 			'typo3' => '4.4.0-7.99.99',
@@ -35,9 +38,6 @@ $EM_CONF[$_EXTKEY] = array(
 		),
 		'suggests' => array(
 		),
-	),
-	'_md5_values_when_last_written' => 'a:27:{s:9:"ChangeLog";s:4:"5c9c";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"c43d";s:14:"ext_tables.php";s:4:"e2cf";s:15:"css/default.css";s:4:"4596";s:16:"css/mainmenu.css";s:4:"0a62";s:15:"css/noright.css";s:4:"bcd3";s:15:"css/submenu.css";s:4:"2e76";s:14:"doc/manual.sxw";s:4:"2e74";s:24:"html/mastertemplate.html";s:4:"5ffb";s:37:"html/mastertemplate_FILES/default.css";s:4:"edb9";s:20:"static/constants.txt";s:4:"f9ec";s:16:"static/setup.txt";s:4:"20b6";s:41:"static/adaptions/tt_content/constants.txt";s:4:"31c2";s:37:"static/adaptions/tt_content/setup.txt";s:4:"8778";s:32:"static/columns/2_3/constants.txt";s:4:"c093";s:28:"static/columns/2_3/setup.txt";s:4:"0ee3";s:30:"static/define/config/setup.txt";s:4:"686a";s:30:"static/define/header/setup.txt";s:4:"9d89";s:32:"static/temp/breadcrumb/setup.txt";s:4:"d9ae";s:28:"static/temp/footer/setup.txt";s:4:"53c2";s:26:"static/temp/left/setup.txt";s:4:"3271";s:26:"static/temp/logo/setup.txt";s:4:"dc38";s:30:"static/temp/mainmenu/setup.txt";s:4:"e56d";s:27:"static/temp/right/setup.txt";s:4:"3389";s:29:"static/temp/submenu/setup.txt";s:4:"42da";}',
-	'suggests' => array(
 	),
 );
 
