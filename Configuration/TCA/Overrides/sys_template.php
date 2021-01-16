d562cdf3571cc58435ebf941929e1f2b11b33f91
Franz Holzinger <holzinger@ttproducts.de>
2021-01-16 17:34:14 +0100

    move call of addStaticFile into the new file Configuration/TCA/Overrides/sys_template.php

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,7 @@
+<?php
+defined('TYPO3_MODE') || die('Access denied.');
+
+call_user_func(function () {
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile( TT_PRODUCTS_EXT, 'Configuration/TypoScript/', 'Master Template Constants Editor');
+});
--- ext_tables.php
+++ ext_tables.php
@@ -1,17 +1,3 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-	die ('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
 
-$callingClassName = '\\TYPO3\\CMS\\Core\\Utility\\ExtensionManagementUtility';
-
-if (
-	class_exists($callingClassName) &&
-	method_exists($callingClassName, 'extPath')
-) {
-	// nothing
-} else {
-	$callingClassName = 't3lib_extMgm';
-}
-
-call_user_func($callingClassName . '::addStaticFile', 'mastertemplate', 'static/master/', 'Master Template Constants Editor');
