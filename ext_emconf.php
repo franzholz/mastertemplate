24073ab689b6f1f89e0fa42b6d42209f236bbe21
Franz Holzinger <holzinger@ttproducts.de>
2021-01-28 20:27:34 +0100

    use [] instead of array()

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,27 +7,27 @@
 # Manual updates
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'Master Template',
-	'description' => 'This extension makes available the setup of the master template. Originally developed by the NRW user group.',
-	'category' => 'templates',
-	'author' => 'Franz Holzinger',
-	'author_email' => 'franz@ttproducts.de',
-	'state' => 'stable',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'clearCacheOnLoad' => 0,
-	'author_company' => 'jambage.com',
-	'version' => '0.5.1',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '9.5.0-10.4.99',
-			'modern_template_building' => '1.0.1-1.0.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-);
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Master Template',
+    'description' => 'This extension makes available the setup of the master template. Originally developed by the NRW user group.',
+    'category' => 'templates',
+    'author' => 'Franz Holzinger',
+    'author_email' => 'franz@ttproducts.de',
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'author_company' => 'jambage.com',
+    'version' => '0.5.1',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-10.4.99',
+            'modern_template_building' => '1.0.1-1.0.99',
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
 
